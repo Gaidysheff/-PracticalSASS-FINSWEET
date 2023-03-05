b07ec3c64262163b6ed3d91f953b1716bd96178b
Gaidysheff <gaidysheff@mail.ru>
2023-03-05 22:22:29 +0500

    slider drafted

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,13 @@ $(function () {
       $(this).next().text(rating);
     },
   });
+
+  // ----------------------- Фильтр блога --------------------------------
+
+  var mixer = mixitup(".blog__list");
+
+  $(".blog__filter-btn").on("click", function () {
+    $(".blog__filter-btn").removeClass("blog__filter-btn--active");
+    $(this).addClass("blog__filter-btn--active");
+  });
 });
