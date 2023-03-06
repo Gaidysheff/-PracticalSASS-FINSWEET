ee7bae8981edec8f9ffa9733795e5eb52d7dbf18
Gaidysheff <gaidysheff@mail.ru>
2023-03-06 10:35:28 +0500

    slider draft-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,32 @@ $(function () {
     },
   });
 
-  // ----------------------- Фильтр блога --------------------------------
+  // ----------------------- Фильтр блога -------------------------------
 
   var mixer = mixitup(".blog__list");
 
   $(".blog__filter-btn").on("click", function () {
     $(".blog__filter-btn").removeClass("blog__filter-btn--active");
     $(this).addClass("blog__filter-btn--active");
+  });
+
+  // --------------------- Слайдер мнения клиентов ----------------------
+
+  $(".customers__slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true,
+    appendDots: $(".customers__dots"),
+    waitForAnimate: false,
+  });
+  $(".customers__prev").on("click", function (e) {
+    e.preventDefault();
+    $(".customers__slider").slick("slickPrev");
+  });
+  $(".customers__next").on("click", function (e) {
+    e.preventDefault();
+    $(".customers__slider").slick("slickNext");
   });
 });
